a0bd47552c71707eff6f09fc9252214d0a177a6f
adrianzysk <adrianzysk1@gmail.com>
2020-05-02 13:13:33 +0200

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+    const [count, setCount] = useState(0)
+    const handleClickPlus = () => {
+       setCount(count + 1)
+    }
+    const handleClickMinus = () => {
+        setCount(count - 1)
+    }
+    return (
+       <div>
+             <button onClick={handleClickPlus}>+</button>
+             <h1>{count}</h1>
+             <button onClick={handleClickMinus}>-</button>
+            {
+                count > 5 && <h1>{'wiêcej ni¿ 5'}</h1>
+            }
+            {
+                count < 0 && <h1>{'mniej ni¿ 0'}</h1>
+            }
+       </div>
+    )
+}
+
+export default App;
